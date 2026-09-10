4ec7854dd1882b4144114c46d074e4d1cba99566
Bravee06 <bravee300@gmail.com>
2026-09-10 23:47:35 +0700

    feat: add Google Site Verification to metadata and public directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,9 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
+  verification: {
+    google: '4i6Gh_PkRaYCyvDs9Mgp8BTCdEoelH1tXxYjEDzOT88',
+  },
 };
 
 export default function RootLayout({
